0d3fecad89f7c7e7542f87feb01e0d4dc53b8733
karolswiderski <karolswiderski@github.com>
2020-04-15 12:04:14 +0200

    add: left-menu.js in Layout

--- car-rental-notes/App_Start/BundleConfig.cs
+++ car-rental-notes/App_Start/BundleConfig.cs
@@ -14,8 +14,6 @@ namespace car_rental_notes
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
@@ -25,6 +23,14 @@ namespace car_rental_notes
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            // ----------------------------------------------------------------------------------------------------
+
+            bundles.Add(new StyleBundle("~/bundles/layout").Include(
+
+                      "~/Scripts/myScripts/Layout/left-menu.js",
+                      "~/Scripts/myScripts/Layout/set-display.js"));
+
         }
     }
 }
--- car-rental-notes/Scripts/myScripts/Layout/left-menu.js
+++ car-rental-notes/Scripts/myScripts/Layout/left-menu.js
@@ -1,7 +1,53 @@
 ﻿function openNavBox(x) {
+    document.getElementById("left-menu-container").style.opacity = "1";
+    document.getElementById("left-menu-container").style.background = "#1c1d22";
 
+    document.getElementById("open-close-left-menu").style.display = "none";
+
+    if (x == 3) {
+        document.getElementById("left-menu-container").style.width = "15%";
+        document.getElementById("page-content").style.marginLeft = "15%";
+    }
+    if (x == 2) {
+        document.getElementById("left-menu-container").style.width = "30%";
+        document.getElementById("page-content").style.marginLeft = "30%";
+    }
+    if (x == 1) {
+        document.getElementById("left-menu-container").style.width = "100%";
+        document.getElementById("left-menu-container").style.height = "100%";
+        document.getElementById("page-content").style.width = "100%";
+    }
+
+    document.getElementById("back-to-content-btn").style.display = "block";
+    document.getElementById("logo-box").style.width = "80%";
+    document.getElementById("left-menu-navigation").style.display = "block";
+    document.getElementById("mmb-ul-element").style.display = "block";
 }
 
+
+
+
 function closeNavBox(x) {
-    
+    document.getElementById("left-menu-container").removeAttribute("onmouseover");
+
+    document.getElementById("left-menu-container").style.opacity = "0.1";
+    document.getElementById("left-menu-container").style.background = "#6b5b95";
+
+    document.getElementById("open-close-left-menu").style.display = "block";
+
+    if (x == 3 || x == 2 || x == 1) {
+        document.getElementById("left-menu-container").style.width = "0.5%";
+        document.getElementById("page-content").style.marginLeft = "0.5%";
+    }
+
+    document.getElementById("back-to-content-btn").style.display = "none";
+    document.getElementById("logo-box").style.width = "0%";
+    document.getElementById("left-menu-navigation").style.display = "none";
+    document.getElementById("mmb-ul-element").style.display = "none";
+
+    setTimeout(function () {
+        document
+            .getElementById("left-menu-container")
+            .setAttribute("onmouseover", "openNav(" + x + ")");
+    }, 500);
 }
