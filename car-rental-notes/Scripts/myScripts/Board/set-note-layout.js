14e82f7faa465780af34215976039f8d2fc641d3
karolswiderski <kswiderski98@gmail.com>
2020-04-18 23:12:09 +0200

    code refactoring: set-note-layout.js

--- car-rental-notes/Scripts/myScripts/Board/set-note-layout.js
+++ car-rental-notes/Scripts/myScripts/Board/set-note-layout.js
@@ -1,42 +1,26 @@
 ﻿function set_note_layout(rodzaj, id) {
-    console.log("11");
+    if (rodzaj == "WYDANIE") {
+        set_colors(id, "#00b383", "#00b383", "#00cd96");
 
+    } else if (rodzaj == "ODBIOR") {
+        set_colors(id, "#cd0037", "#cd0037", "#e7003e");
 
-    if (rodzaj == "WYDANIE") { // si
-        console.log("jestem w wydanie");
-        $("#" + id + " .front-note-side").css("background-color", "#00b383");
-        $("#" + id + " .back-note-side").css("background-color", "#00b383");
-        $("#" + id + " .back-side-menu").css("background-color", "#00cd96");
+    } else if (rodzaj == "PODMIANA") {
+        set_colors(id, "#008ab3", "#008ab3", "#009ecd");
 
-    } else if (rodzaj == "ODBIOR") { // ?
-        console.log("jestem w odbiór");
-        $("#" + id + " .front-note-side").css("background-color", "#cd0037");
-        $("#" + id + " .back-note-side").css("background-color", "#cd0037");
-        $("#" + id + " .back-side-menu").css("background-color", "#e7003e");
+    } else if (rodzaj == "SERWIS") {
+        set_colors(id, "#b38300", "#b38300", "#cd9600");
 
-    } else if (rodzaj == "PODMIANA") { // ?
-        console.log("jestem w podmiana");
-        $("#" + id + " .front-note-side").css("background-color", "#008ab3");
-        $("#" + id + " .back-note-side").css("background-color", "#008ab3");
-        $("#" + id + " .back-side-menu").css("background-color", "#009ecd");
-
-    } else if (rodzaj == "SERWIS") { // si
-        console.log("jestem w serwis");
-        $("#" + id + " .front-note-side").css("background-color", "#b38300");
-        $("#" + id + " .back-note-side").css("background-color", "#b38300");
-        $("#" + id + " .back-side-menu").css("background-color", "#cd9600");
-
-    } else if (rodzaj == "INNE") { // si
-        console.log("jestem w inne");
-        $("#" + id + " .front-note-side").css("background-color", "#8ab300");
-        $("#" + id + " .back-note-side").css("background-color", "#8ab300");
-        $("#" + id + " .back-side-menu").css("background-color", "#9ecd00");
+    } else if (rodzaj == "INNE") {
+        set_colors(id, "#8ab300", "#8ab300", "#9ecd00");
 
     } else if (rodzaj == "OPONY") {
-        console.log("jestem w iopony");
-        $("#" + id + " .front-note-side").css("background-color", "#616582");
-        $("#" + id + " .back-note-side").css("background-color", "#616582");
-        $("#" + id + " .back-side-menu").css("background-color", "#6c7091");
-
+        set_colors(id, "#616582", "#616582", "#6c7091");
     }
 };
+
+function set_colors(id,c1,c2,c3) {
+    $("#" + id + " .front-note-side").css("background-color", c1);
+    $("#" + id + " .back-note-side").css("background-color", c2);
+    $("#" + id + " .back-side-menu").css("background-color", c3);
+}
